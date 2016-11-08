b3f1c09a37273e45a8cd80e7b1b921ac7dcaff94
cthbst <edison1998402@gmail.com>
2016-11-08 20:47:27 +0800

    modify Linear Prime

--- codebook/Math/LinearPrime.cpp
+++ codebook/Math/LinearPrime.cpp
@@ -1,10 +1,13 @@
-int p[MAXN], np = 0;
-int a[MAXN];
-
-for ( int i = 2 ; i < n ; i++ ) {
-  if ( a[i] == 0 ) p[np++] = i;
-  for ( int j = 0; j < np && i * p[j] < n ; j++ ) {
-    a[i * p[j]] = 1;
-    if ( i % p[j] == 0) break;
-  }
+const int MAXP = 100; //max prime 
+vector<int> P;	// primes
+void build_prime(){
+	static bitset<MAXP> ok;
+	int np=0;
+	for (int i=2; i<MAXP; i++){
+		if (ok[i]==0)P.push_back(i), np++;
+		for (int j=0; j<np && i*P[j]<MAXP; j++){
+			ok[ i*P[j] ] = 1;
+			if ( i%P[j]==0 )break;
+		}
+	}
 }
