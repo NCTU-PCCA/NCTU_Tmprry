48d60d4488c07048e190332ce4a91be2128aa17b
cthbst <edison1998402@gmail.com>
2017-04-28 15:21:49 +0800

    add DigitCounting

--- codebook/Basic/default.cpp
+++ codebook/Basic/default.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 
 int main(){
+#ifndef AC
+	freopen("","r",stdin);
+#endif
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 }
---
+++ codebook/Other/STL.cpp
@@ -0,0 +1,18 @@
+// algorithm
+random_shuffle(a,a+n);
+next_permutation(a,a+n); // need sort
+nth_element (a, a+k, a+n); // kth
+*min_element(a,a+n);
+*unique(a,a+n); // need sort
+stable_sort(a,a+n); // merge sort
+
+// bitset (s[0] is right most)
+operator[] // 
+count() // count number of 1
+set() // all to 1
+set(k) //   s[k] to 1
+set(k,0) // s[k] to 0
+flip()  // all flip
+flip(k) // s[k] flip 
+to_ulong()
+to_string()
