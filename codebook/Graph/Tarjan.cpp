e951f212a3ad948576a0b4b3f3f0ac066a0e18e5
吳宗達 <edison1998402@gmail.com>
2017-07-11 14:01:59 +0800

    Update Tarjan.cpp
    Tarjan SCC to struct

--- codebook/Graph/Tarjan.cpp
+++ codebook/Graph/Tarjan.cpp
@@ -1,39 +1,46 @@
-int n;
-vector<int> G[MAXN];
-stack<int> stk;
-int dfn[MAXN], low[MAXN];
-bool ins[MAXN];
-int scc[MAXN], scn, count;
+// 0 base
+struct TarjanSCC{
+	static const int MAXN = 1000006;
+	int n, dfn[MAXN], low[MAXN], scc[MAXN], scn, count;
+	vector<int> G[MAXN];
+	stack<int> stk;
+	bool ins[MAXN];
 
-void tarjan(int u){
-  dfn[u] = low[u] = ++count;
-  stk.push(u);
-  ins[u] = true;
+	void tarjan(int u){
+		dfn[u] = low[u] = ++count;
+		stk.push(u);
+		ins[u] = true;
 
-  for(auto v:G[u]){
-    if(!dfn[v]){
-      tarjan(v);
-      low[u] = min(low[u], low[v]);
-    }else if(ins[v]){
-      low[u] = min(low[u], dfn[v]);
-    }
-  }
+		for(auto v:G[u]){
+			if(!dfn[v]){
+				tarjan(v);
+				low[u] = min(low[u], low[v]);
+			}else if(ins[v]){
+				low[u] = min(low[u], dfn[v]);
+			}
+		}
 
-  if(dfn[u] == low[u]){
-    int v;
-    do {
-      v = stk.top();
-      stk.pop();
-      scc[v] = scn;
-      ins[v] = false;
-    } while(v != u);
-    scn++;
-  }
-}
+		if(dfn[u] == low[u]){
+			int v;
+			do {
+			v = stk.top();
+			stk.pop();
+			scc[v] = scn;
+			ins[v] = false;
+			} while(v != u);
+			scn++;
+		}
+	}
 
-void GetSCC(){
-  count = scn = 0;
-  for(int i = 0 ; i < n ; i++ ){
-    if(!dfn[i]) tarjan(i);
-  }
-}
+	void getSCC(){
+		memset(dfn,0,sizeof(dfn));
+		memset(low,0,sizeof(low));
+		memset(ins,0,sizeof(ins));
+		memset(scc,0,sizeof(scc));
+		count = scn = 0;
+		for(int i = 0 ; i < n ; i++ ){
+			if(!dfn[i]) tarjan(i);
+		}
+	}
+
+}SCC;
