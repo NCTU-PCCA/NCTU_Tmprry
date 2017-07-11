b4271fd001d1f3b5f8ae80c75a9b674fb73fa179
吳宗達 <edison1998402@gmail.com>
2017-07-11 15:49:03 +0800

    Update ConvexHull.cpp

--- codebook/Geometry/ConvexHull.cpp
+++ codebook/Geometry/ConvexHull.cpp
@@ -1,72 +1,18 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include "2Dpoint.cpp"
 
-typedef long long LL;
-const int MAXN = 100005;
-const LL INF = (1LL)<<62;
-
-struct Point{
-    LL x, y;
-    Point (LL x=0, LL y=0):x(x),y(y){}
-    bool operator < (const Point &B)const {
-        if (x!=B.x)return x<B.x;
-        return y<B.y;
+// retunr H, 第一個點會在 H 出現兩次
+void ConvexHull(vector<Point> &P, vector<Point> &H){
+    int n = P.size(), m=0;
+    sort(P.begin(),P.end());
+    H.clear();
+    
+    for (int i=0; i<n; i++){
+        while (m>=2 && (P[i]-H[m-2]) % (H[m-1]-H[m-2]) <0)H.pop_back(), m--;
+        H.push_back(P[i]), m++;
     }
-    Point operator - (Point B){
-        return Point(x-B.x,y-B.y);
+
+    for (int i=n-2; i>=0; i--){
+        while (m>=2 && (P[i]-H[m-2]) % (H[m-1]-H[m-2]) <0)H.pop_back(), m--;
+        H.push_back(P[i]), m++;
     }
-};
-LL cross(Point A, Point B){
-    return A.x*B.y-A.y*B.x;
-}
-LL Abs(LL x){
-    return x>0?x:-x;
-}
-LL AreaU[MAXN], AreaD[MAXN];
-void find_CH(int N, Point P[], LL Area[]){
-    static vector<Point> U, D;
-    static vector<int> Ui, Di;
-    U.clear(), Ui.clear();
-    D.clear(), Di.clear();
-    int uz=0, dz=0;
-
-    for (int i=0; i<N; i++){
-        while (uz>=2 && cross(P[i]-U[uz-2],U[uz-1]-U[uz-2])<=0)U.pop_back(), Ui.pop_back(), uz--;
-        if (uz<=1)AreaU[i]=0;
-        else AreaU[i] = AreaU[ Ui[uz-1] ] + Abs(cross(P[i]-U[0],U[uz-1]-U[0]));
-        U.push_back(P[i]),Ui.push_back(i),uz++;
-
-        while (dz>=2 && cross(P[i]-D[dz-2],D[dz-1]-D[dz-2])>=0)D.pop_back(), Di.pop_back(), dz--;
-        if (dz<=1)AreaD[i]=0;
-        else AreaD[i] = AreaD[ Di[dz-1] ] + Abs(cross(P[i]-D[0],D[dz-1]-D[0]));
-        D.push_back(P[i]),Di.push_back(i),dz++;
-
-        Area[i] = AreaU[i] + AreaD[i];
-        //printf("Area[%d]=%lld\n",i ,Area[i]);
-    }
-    //puts("");
-}
-
-int N;
-Point P[MAXN];
-LL AreaL[MAXN], AreaR[MAXN];
-
-int main(){
-    input();
-
-    find_CH(N,P,AreaL);
-    for (int i=0; i<N; i++)P[i].x*=-1;
-    reverse(P,P+N);
-    find_CH(N,P,AreaR);
-    reverse(AreaR,AreaR+N);
-    reverse(P,P+N);
-
-    LL Ans = min(AreaL[N-1],AreaR[0]);
-    for (int i=0; i<N-1; i++){
-        if (P[i].x!=P[i+1].x){
-            Ans = min (Ans,AreaL[i]+AreaR[i+1]);
-        }
-    }
-	if (P[0].x==P[N-1].x)Ans=0;
-    printf("%lld\n",(Ans+1)/2LL);
 }
