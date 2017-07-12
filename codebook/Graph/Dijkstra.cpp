d60891dadab5ceca8c28404ac66fa9d1e2af77be
吳宗達 <edison1998402@gmail.com>
2017-07-12 13:59:33 +0800

    Update Dijkstra.cpp

--- codebook/Graph/Dijkstra.cpp
+++ codebook/Graph/Dijkstra.cpp
@@ -1,34 +1,25 @@
-// 1 base
-struct DijkstraTemplate{
-typedef long long LL;
-typedef pair<LL,int> PLI;
-typedef pair<int,LL> PIL; // (to, dis)
-static const int MAXN=50005;
-static const LL INF=1LL<<62;
-int n, m;
-vector<PIL> G[MAXN];    // graph 
-LL d[MAXN];             // answer
-void init(){
-    for (int i=0; i<MAXN; i++){
-        G[i].clear();
+typedef struct Edge{
+    int v; long long len;
+    bool operator > (const Edge &b)const { return len>b.len; }
+} State;
+
+const long long INF = 1LL<<60;
+
+void Dijkstra(int n, vector<Edge> G[], long long d[], int s, int t=-1){
+    static priority_queue<State, vector<State>, greater<State> > pq;
+    while ( pq.size() )pq.pop();
+    for (int i=1; i<=n; i++)d[i]=INF;
+    d[s]=0; pq.push( (State){s,d[s]} );
+    while ( pq.size() ){
+        auto x = pq.top(); pq.pop();
+        int u = x.v;
+        if (d[u]<x.len)continue;
+        if (u==t)return;
+        for (auto &e:G[u]){
+            if (d[e.v] > d[u]+e.len){
+                d[e.v] = d[u]+e.len;
+                pq.push( (State) {e.v,d[e.v]} );
+            }
+        }
     }
 }
-bool dijkstra(int from,int to){
-	priority_queue<PLI,vector<PLI>,greater<PLI> > pq;
-	for(int i=1;i<=n;++i)d[i]=INF;
-	d[from]=0;
-	pq.push(make_pair(d[from],from));
-	while(pq.size()){
-		PLI u=pq.top(); pq.pop();
-		int x=u.second;
-		if(d[x]<u.first)continue;
-		for(size_t i=0;i<G[x].size();++i){
-			if(d[G[x][i].first]>d[x]+G[x][i].second){
-				d[G[x][i].first]=d[x]+G[x][i].second;
-				pq.push(make_pair(d[G[x][i].first],G[x][i].first));
-			}
-		}
-	}
-	return d[to]==INF?0:1;  //有/無解 
-}
-}Dijkstra;
