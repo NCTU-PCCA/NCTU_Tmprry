c12f9cac1082d94024af9b8cd9b77e165660517a
吳宗達 <edison1998402@gmail.com>
2017-07-13 22:56:45 +0800

    Update Dijkstra.cpp

--- codebook/Graph/Dijkstra.cpp
+++ codebook/Graph/Dijkstra.cpp
@@ -5,7 +5,7 @@ typedef struct Edge{enileni
 
 const long long INF = 1LL<<60;
 
-void Dijkstra(int n, vector<Edge> G[], long long d[], bool vis[], int s, int t=-1){
+void Dijkstra(int n, vector<Edge> G[], long long d[], int s, int t=-1){
     static priority_queue<State, vector<State>, greater<State> > pq;
     while ( pq.size() )pq.pop();
     for (int i=1; i<=n; i++)d[i]=INF, vis[i]=0;
@@ -13,7 +13,7 @@ void Dijkstra(int n, vector<Edge> G[], long long d[], bool vis[], int s, int t=-
     while ( pq.size() ){
         auto x = pq.top(); pq.pop();
         int u = x.v;
-        if (vis[u])continue;  vis[u]=1;
+        if (d[u]<x.len)continue;
         if (u==t)return;
         for (auto &e:G[u]){
             if (d[e.v] > d[u]+e.len){
