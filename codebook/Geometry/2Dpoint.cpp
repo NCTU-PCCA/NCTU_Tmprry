aebf2494a9538aa9b40cbf832df1bcef9b22f20c
cthbst <edison1998402@gmail.com>
2017-09-14 00:03:51 +0800

    update 2Dpoint.cpp

--- codebook/Geometry/2Dpoint.cpp
+++ codebook/Geometry/2Dpoint.cpp
@@ -4,10 +4,14 @@ struct Point {
 	Point (T _x=0, T _y=0):x(_x),y(_y){}
 
 	bool operator < (const Point &b)const{
-		return atan2(y,x) < atan2(b.y,b.x);
+		//return tie(x,y) < tie(b.x,b.y);
+		//return atan2(y,x) < atan2(b.y,b.x);
+		assert(0 && "choose compare");
 	}
 	bool operator == (const Point &b)const{
-		return atan2(y,x) == atan2(b.y,b.x);
+		//return tie(x,y) == tie(b.x,b.y);
+		//return atan2(y,x) == atan2(b.y,b.x);
+		assert(0 && "choose compare");
 	}
 	Point operator + (const Point &b)const{
 		return Point(x+b.x,y+b.y);
@@ -24,10 +28,10 @@ struct Point {
 	Point operator * (const T &d)const{
 		return Point(d*x,d*y);
 	}
-	T abs2() { return x*x+y*y; }
-	T abs() { return sqrt( abs2() ); }
+	double abs2() { return x*x+y*y; }
+	double abs() { return sqrt( abs2() ); }
 };
 typedef Point pdd;
-inline double abs2(pdd a){
+double abs2(pdd a){
 	return a.abs2();
 }
