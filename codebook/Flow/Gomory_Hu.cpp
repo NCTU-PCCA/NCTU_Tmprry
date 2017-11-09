36e5e38d82861d354748683944eb5f4b6dfaa74c
Yuxin Zhou <yqkqknct@gmail.com>
2017-11-09 21:52:15 +0800

    polish Gomory_Hu

--- codebook/Flow/Gomory_Hu.cpp
+++ codebook/Flow/Gomory_Hu.cpp
@@ -10,15 +10,22 @@ Construct of Gomory Hu Tree
 
 int e[MAXN][MAXN];
 int p[MAXN]; 
+
+Dinic D; // original graph
+
 void gomory_hu() {
     fill(p, p+n, 0);
-    fill(e[0], e[n], -1);
-    for ( int i = 1 ; i < n ; i++ ) {
-        init(n); // the first step
-        int tmp = flow.max_flow(i, p[i]);
-        e[i][p[i]] = e[p[i]][i] = tmp;
-        for ( int j = i + 1 ; j < n ; j++ ) {
-            if ( flow.used[j] && p[i] == p[j] ) p[j] = i;
-        }
+    fill(e[0], e[n], INF);
+    for ( int s = 1 ; s < n ; s++ ) {
+        int t = p[s];
+        Dinic F = D;
+        int tmp = F.max_flow(s, t);
+
+        for ( int i = 1 ; i < s ; i++ )
+            e[s][i] = e[i][s] = min(tmp, e[t][i]);
+
+        for ( int i = s+1 ; i <= n ; i++ ) 
+            if ( p[i] == t && F.side[i] ) p[i] = s;
+
     }
 }
